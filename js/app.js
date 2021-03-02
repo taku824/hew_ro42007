daf6c92f6a3f632a0ac993e92de8f54eeb799354
Fujisaki Taku <74420472+taku824@users.noreply.github.com>
2021-03-02 10:35:28 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const $button = document.getElementsById('button');
+
+const clickHandler = (e) => {
+  if(e.target.id === button1 || e.target.id === button2 || e.target.id === button2){
+      window.alert('準備中...');
+  }
+
+};
